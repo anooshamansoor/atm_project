f6a0d2bbcf9b64bfb7335d93be89aee794dde244
anooshamansoor <160314532+anooshamansoor@users.noreply.github.com>
2024-05-08 01:27:04 +0500

    push my calculator to github

---
+++ main.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currentbalance = 10000;
+console.log("your current balance is " + currentbalance);
+let myBalance = 10000;
+let mypin = 9988;
+let pinanswer = await inquirer.prompt([
+    { name: "pin",
+        message: "enter your pin",
+        type: "number",
+    },
+]);
+if (pinanswer.pin === mypin) {
+    console.log("correct pin code!");
+    let operationanswer = await inquirer.prompt([
+        { name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"] }
+    ]);
+    if (operationanswer.operation === "withdraw") {
+        let amountanswer = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountanswer.amount;
+        console.log("your remaining balance is " + myBalance);
+    }
+    else if (operationanswer.operation === "check balance") {
+        console.log("your current balance is " + myBalance);
+    }
+}
+else {
+    console.log("incorrect pin");
+}
+;
